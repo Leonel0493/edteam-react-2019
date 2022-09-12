6af5eac262dfef7406422c8e891b80ab18e94068
Leonel Rivas <leonrivas04@gmail.com>
2022-09-12 10:43:56 -0600

    Class 4
    Learned about components and props

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,35 +1,48 @@
-const Curso = () => {
+import image404 from "./Images/Svg/404-error.svg";
+import PropTypes from "prop-types";
+
+const Curso = ({ title, courseImage, teacher, teacherImage, price }) => {
   return (
     <article className="card">
       <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-        <img
-          src="https://www.cultture.com/pics/2021/01/naruto-10-de-los-jutsu-mas-dificiles-de-aprender-7.jpg"
-          alt="Ninjutso banner"
-        />
+        <img src={courseImage} alt={title} />
       </div>
       <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-        <h3 className="t5 s-mb-2 s-center">Ninjutso</h3>
+        <h3 className="t5 s-mb-2 s-center">{title}</h3>
         <div className="s-mb-2 s-main-center">
           <div className="card__teacher s-cross-center">
             <div className="card__avatar s-mr-1">
               <div className="circle img-container">
-                <img
-                  src="https://image.winudf.com/v2/image/Y29tLmFuZHJvbW8uZGV2NjYwNjE0LmFwcDcxMjI2NF9zY3JlZW5fMF8xNTEzNTEzMjU4XzA3NA/screen-0.jpg?fakeurl=1&type=.webp"
-                  alt="Sakura Harumo profile"
-                />
+                <img src={teacherImage} alt={teacher} />
               </div>
             </div>
-            <span className="small">Sakura Harumo</span>
+            <span className="small">{teacher}</span>
           </div>
         </div>
         <div className="s-main-center">
           <a className="button--ghost-alert button--tiny" href="/home">
-            $ 35USD
+            {`$ ${price} USD`}
           </a>
         </div>
       </div>
     </article>
   );
+};
+
+Curso.propTypes = {
+  title: PropTypes.string,
+  courseImage: PropTypes.string,
+  teacher: PropTypes.string,
+  teacherImage: PropTypes.string,
+  price: PropTypes.number,
+};
+
+Curso.defaultProps = {
+  title: "Title not loaded",
+  courseImage: image404,
+  teacher: "Teacher nor found",
+  teacherImage: image404,
+  price: 0,
 };
 
 export default Curso;
